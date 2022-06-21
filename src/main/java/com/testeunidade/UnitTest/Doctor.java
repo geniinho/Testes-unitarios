4391ef0d6a82b7dd766d00b0130528c2b5509608
geniinho <geniinho@hotmail.com>
2022-06-21 10:42:39 -0300

    import do repositorio base

---
+++ src/main/java/com/testeunidade/UnitTest/Doctor.java
@@ -0,0 +1,17 @@
+package com.testeunidade.UnitTest;
+
+public enum Doctor {
+    avery("Ralph Avery"),
+    johnson("Beth Johnson"),
+    murphy("Pat Murpy");
+
+    private String name;
+
+    Doctor(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
